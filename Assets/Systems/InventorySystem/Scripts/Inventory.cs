106a552c09794bd5abac747dd7353a9e407f320e
Felixnm1 <felixn.marti@gmail.com>
2026-06-23 01:13:05 +0200

    Puertas con llave
    Ahora cada puerta responde a una llave en el inventario

--- Assets/Systems/InventorySystem/Scripts/Inventory.cs
+++ Assets/Systems/InventorySystem/Scripts/Inventory.cs
@@ -8,23 +8,13 @@ public class Inventory : MonoBehaviour
 
     public UnityEvent<InventoryInfo> onObjectUsed;
 
-    //List<InventoryInfo> itemsInInventory = new List<InventoryInfo>();
+    
 
     public void NotifyObjectUsed(InventoryInfo invInfo)
     {
         this.onObjectUsed.Invoke(invInfo);
-    }/*
-    public void addItem(InventoryInfo invinf)
-    {
-        itemsInInventory.Add(invinf);
     }
-    public void removeItem(InventoryInfo invinf)
-    {
-        itemsInInventory.Remove(invinf);
-    }
-    public bool checkItem(InventoryInfo invinf)
-    {
-        return itemsInInventory.Contains(invinf);
-    }*/
+
+    
 
 }
